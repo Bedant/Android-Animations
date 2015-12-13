c217c819291d54d804f28b727a33afbe12c33af0
Bedant <bebi2527@live.in>
2015-12-13 18:55:58 +0530

    card animation

--- app/src/main/java/com/example/bebi2/androidanimation/MainActivity.java
+++ app/src/main/java/com/example/bebi2/androidanimation/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.bebi2.androidanimation;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +25,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        context = this;
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+
+        final CardView card1 = (CardView) findViewById(R.id.card1);
+        final CardView card2 = (CardView) findViewById(R.id.card2);
+        final CardView card3 = (CardView) findViewById(R.id.card3);
+
+        final ImageView image1 = (ImageView) findViewById(R.id.img1);
+        final ImageView image2 = (ImageView) findViewById(R.id.img2);
+        final ImageView image3 = (ImageView) findViewById(R.id.img3);
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Main2Activity.class);
+
+                Pair<View, String> p2 = Pair.create((View)card1, "card");
+                Pair<View, String> p1 = Pair.create((View)image1, "cover");
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(MainActivity.this, p2, p1);
+                startActivity(intent, options.toBundle());
+
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Main2Activity.class);
+                Pair<View, String> p1 = Pair.create((View)image2, "cover");
+                Pair<View, String> p2 = Pair.create((View)card2, "card");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(MainActivity.this, p1, p2);
+                startActivity(intent, options.toBundle());
+
+            }
+        });
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Main2Activity.class);
+                Pair<View, String> p1 = Pair.create((View)image3, "cover");
+                Pair<View, String> p2 = Pair.create((View)card3, "card");
+
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(MainActivity.this, p1, p2);
+                startActivity(intent, options.toBundle());
+
             }
         });
     }
@@ -49,4 +107,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
